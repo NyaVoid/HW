cb5ed8e3e80a63f3037bf7a1f8123d3e8fed7346
NyaVoid <162377934+NyaVoid@users.noreply.github.com>
2024-06-07 14:57:56 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,5 @@
+def divide(x, y):
+    if y == 0:
+        return 'error'
+    else:
+        return x / y
---
+++ true_math.py
@@ -0,0 +1,5 @@
+def divide(x, y):
+    if y == 0:
+        return 'inf'
+    else:
+        return x / y 
