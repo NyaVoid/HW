10e6484f35f191f7ee3ce57015f394e07f07e51e
NyaVoid <162377934+NyaVoid@users.noreply.github.com>
2024-08-02 13:31:12 +0300

    Update hw.py

--- hw.py
+++ hw.py
@@ -14,8 +14,13 @@ def write_words(word_count, file_name):
 def run_something(word_count, file_name):
     t = threading.Thread(target=write_words, args=(word_count, file_name))
     t.start()
-
-
+s_time = time.time()
+write_words(10, 'example1.txt')
+write_words(30, 'example2.txt')
+write_words(200, 'example3.txt')
+write_words(100, 'example4.txt')
+e_time = time.time()
+print(e_time - s_time)
 start_time = time.time()
 run_something(10, 'exemple5.txt')
 run_something(30, 'exemple6.txt')
