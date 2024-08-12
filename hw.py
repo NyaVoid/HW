1ae6ecf28744c350390cb1d53ae77854df2dcf05
NyaVoid <162377934+NyaVoid@users.noreply.github.com>
2024-08-13 01:05:36 +0300

    Create hw.py

---
+++ hw.py
@@ -0,0 +1,10 @@
+import time
+import os
+for root, dirs, files in os.walk('/home/overwii/Documents'):
+  for file in files:
+    filepath = os.path.join(root, file)
+    filetime = os.path.getmtime(filepath)
+    formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+    filesize = os.path.getsize(filepath)
+    parent_dir = os.path.dirname(file)
+    print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
