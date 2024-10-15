cd5f3cd84ded377d661d88883cdcb82141c0ef07
Nameless <162377934+NyaVoid@users.noreply.github.com>
2024-10-16 00:08:30 +0300

    Create hw.py

---
+++ meh4/hw.py
@@ -0,0 +1,53 @@
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher.filters import Text
+
+API_TOKEN = 'token'
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+@dp.message_handler(Text(equals='Calories', ignore_case=True), state='*')
+async def set_age(message: types.Message):
+    await message.reply("Введите свой возраст:")
+    await UserState.age.set()
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message: types.Message, state: FSMContext):
+    await state.update_data(age=int(message.text))
+    await message.reply("Введите свой рост (в см):")
+    await UserState.growth.set()
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message: types.Message, state: FSMContext):
+    await state.update_data(growth=int(message.text))
+    await message.reply("Введите свой вес (в кг):")
+    await UserState.weight.set()
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message: types.Message, state: FSMContext):
+    await state.update_data(weight=int(message.text))
+    data = await state.get_data()
+
+    age = data['age']
+    growth = data['growth']
+    weight = data['weight']
+
+    calories = 10 * weight + 6.25 * growth - 5 * age - 161
+
+    await message.reply(f"Ваша норма калорий: {calories:.2f} ккал в день.")
+    await state.finish()
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
