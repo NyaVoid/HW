49495667efd8c255330cce4802b53f6608d7c4e6
Nameless <162377934+NyaVoid@users.noreply.github.com>
2024-10-29 12:54:32 +0300

    Update hw.py

--- meh12/hw.py
+++ meh12/hw.py
@@ -5,6 +5,7 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.dispatcher.filters import Text 
+from crud_functions import initiate_db, get_all_products
 
 API_TOKEN = 'token'
 
@@ -19,6 +20,8 @@ class UserState(StatesGroup):
     growth = State()
     weight = State()
 
+initiate_db()
+
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
@@ -30,28 +33,30 @@ async def start(message: types.Message):
 
 @dp.message_handler(Text(equals='Купить', ignore_case=True))
 async def get_buying_list(message: types.Message):
-    products = [
-        {"name": "Product1", "description": "описание 1", "price": 100},
-        {"name": "Product2", "description": "описание 2", "price": 200},
-        {"name": "Product3", "description": "описание 3", "price": 300},
-        {"name": "Product4", "description": "описание 4", "price": 400},
-    ]
+    products = get_all_products()
+
+    if not products:
+        await message.reply("Нет доступных продуктов.")
+        return
 
     for product in products:
+        title, description, price = product
         await message.reply(
-            f'Название: {product["name"]} | Описание: {product["description"]} | Цена: {product["price"]}'
+            f'Название: {title} | Описание: {description} | Цена: {price}'
         )
 
     inline_keyboard = InlineKeyboardMarkup(row_width=2)
     for product in products:
-        button = InlineKeyboardButton(product["name"], callback_data='product_buying')
+        title = product[0]
+        button = InlineKeyboardButton(title, callback_data=f'product_buying_{title}')
         inline_keyboard.add(button)
 
     await message.reply("Выберите продукт для покупки:", reply_markup=inline_keyboard)
 
-@dp.callback_query_handler(lambda call: call.data == 'product_buying')
+@dp.callback_query_handler(lambda call: call.data.startswith('product_buying_'))
 async def send_confirm_message(call: types.CallbackQuery):
-    await bot.send_message(call.message.chat.id, "Вы успешно приобрели продукт!")
+    product_name = call.data.split('_')[2]
+    await bot.send_message(call.message.chat.id, f"Вы успешно приобрели продукт: {product_name}!")
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
